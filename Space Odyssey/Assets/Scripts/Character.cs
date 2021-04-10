52ceb6930cb8aecbb6a69985fec0ce320cc077fc
MarieSilBaq <58976736+Marie-Sil@users.noreply.github.com>
2021-04-10 15:42:18 +0200

    retour en arrière

--- Space Odyssey/Assets/Scripts/Character.cs
+++ Space Odyssey/Assets/Scripts/Character.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Subtegral.DialogueSystem.DataContainers;
-using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
     public Sprite characterImage;
     public string characterName;
-    //public DialogueParser2 characterDialogue;
-    public Text nameBox;
+    public DialogueParser2 characterDialogue;
 
     // Start is called before the first frame update
     void Start()
     {
-        nameBox.text = characterName;
-
-        GameObject character = GameObject.Find(characterName);
-        SpriteRenderer currSprite = GetComponent<SpriteRenderer>();
-        currSprite.sprite = characterImage;
+        
     }
 
     // Update is called once per frame
